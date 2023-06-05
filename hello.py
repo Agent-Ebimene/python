9b46b939b433db459721c8b9f21420c212ee3966
unknown <Agent Ebimene>
2023-06-05 15:55:08 -0700

    Learn python basics

---
+++ hello.py
@@ -0,0 +1,24 @@
+print("Hello world")
+# Numbers in Python
+age=30
+print(age)
+height=1.76
+hello="Hey you there"
+print(hello)
+#Booleans
+is_logged=True
+is_fit=False
+print(is_fit,is_logged)
+
+#Lists
+schools=["NDU","Cambridge","Otuoke"]
+print(schools)
+
+#Dictionaries are like objects in javascript
+
+person ={"name":"John", "age":30}
+print(person["name"])
+#Turfles are similar to lists but are immutable after they are created
+
+coordinates=(0,1,2,3,4,5,5,5)
+print(coordinates.count(5))
